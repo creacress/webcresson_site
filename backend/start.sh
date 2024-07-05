349e723c28516efe17fab1b0dc01bb25b6e4b892
WebCresson <alexis@webcresson.com>
2024-07-05 17:16:28 +0200

    start.sh update

--- backend/start.sh
+++ backend/start.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# Afficher les fichiers et répertoires actuels pour débogage
+echo "Contenu du répertoire /app :"
+ls -la /app
+
 # Vérifier si le dossier migrations existe, sinon l'initialiser
 if [ ! -d "migrations" ]; then
+  echo "Dossier migrations non trouvé, initialisation..."
   flask db init
   flask db migrate -m "Initial migration"
+else
+  echo "Dossier migrations trouvé, pas besoin d'initialisation."
 fi
 
 # Appliquer les migrations
