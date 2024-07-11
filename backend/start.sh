fef6ab3873d3bbf71cd5326c08a9b76a94b65d01
WebCresson <alexis@webcresson.com>
2024-07-11 11:07:26 +0200

    start sh upgrade

--- backend/start.sh
+++ backend/start.sh
@@ -4,21 +4,23 @@
 echo "Contenu du répertoire /app :"
 ls -la /app
 
-# Vérifier si le dossier migrations existe, sinon l'initialiser
-if [ ! -d "migrations" ]; then
-  echo "Dossier migrations non trouvé, initialisation..."
-  flask db init
-  flask db migrate -m "Initial migration"
-else
-  echo "Dossier migrations trouvé, pas besoin d'initialisation."
+# Supprimer le dossier migrations
+if [ -d "migrations" ]; then
+  rm -rf migrations
+  echo "Dossier migrations supprimé."
 fi
 
-# Afficher les fichiers et répertoires actuels pour débogage après la tentative de création des migrations
-echo "Contenu du répertoire /app après tentative de création des migrations:"
-ls -la /app
+# Initialiser les migrations
+echo "Initialisation des migrations..."
+flask db init
+flask db migrate -m "Initial migration"
 
 # Appliquer les migrations
 flask db upgrade
+
+# Afficher les fichiers et répertoires actuels pour débogage après la création des migrations
+echo "Contenu du répertoire /app après création des migrations:"
+ls -la /app
 
 # Démarrer l'application avec Gunicorn
 exec gunicorn --bind 0.0.0.0:5000 wsgi:app
