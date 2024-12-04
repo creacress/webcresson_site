1f853cdf3c803eaf838f848e51c849bb5f4aacc6
CRESSON Alexis <alexis@webcresson.com>
2024-12-04 09:44:18 +0100

    Reset

--- frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.scss';
-import Root from './App';
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<Root />);
